e3e58598bf163fbcea8c6e8c467960efa44e8da6
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-19 06:35:35 -0700

    have it printing out pairs

--- src/sum_pairs/core.clj
+++ src/sum_pairs/core.clj
@@ -4,26 +4,18 @@
 (defstruct pair :i :j :sum)
 
 (defn pair-key-val [key val]
-  (struct pair key val (+ key val))
-  )
+  (struct pair key val (+ key val)))
 
 (defn create-pairs [vals, index, pairs]
   (def current (nth vals index))
-(def new-pairs (reduce conj pairs
-        (map (partial pair-key-val current) vals)
-        ))
-  (println current)
-(if (= (- (count vals) 1) index)
-  pairs
-  (create-pairs vals (+ index 1) new-pairs)
-  )
-)
-
-                  // I want to store as a map of pair to values
+  (def new-pairs (reduce conj pairs
+                         (map (partial pair-key-val current) vals)))
+  (if (= (- (count vals) 1) index)
+    pairs
+    (create-pairs vals (+ index 1) new-pairs)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (def input [1 3 2 6 1 2])
-
-  (println "Hello, World!"))
+  (println (create-pairs [1 2 3] 0 [])))
