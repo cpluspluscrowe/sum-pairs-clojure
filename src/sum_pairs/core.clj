4c86370cd71c24a1453c0200200a3a7b451f930e
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-19 08:45:54 -0700

    added happy path test

--- src/sum_pairs/core.clj
+++ src/sum_pairs/core.clj
@@ -1,4 +1,5 @@
 (ns sum-pairs.core
+  (:use clojure.test)
   (:gen-class))
 
 (defn pair-key-val [key val]
@@ -20,15 +21,20 @@
     0))
 
 (defn add-divisibles [k pairs]
-  (reduce + (map (partial is-mod-k k) pairs))
-  )
+  (reduce + (map (partial is-mod-k k) pairs)))
+
+(deftest happy-path
+  (def input [1 3 2 6 1 2])
+  (def k 3)
+  (def pairs (create-pairs input 0 []))
+  (is (= pairs) 5))
 
 (defn -main
   "Solves hackerrank problem Sum Pairs"
   [& args]
+  (run-all-tests)
   (def input [1 3 2 6 1 2])
   (def k 3)
   (def pairs (create-pairs input 0 []))
-  (println (add-divisibles k pairs))
-  )
+  (println (add-divisibles k pairs)))
 
