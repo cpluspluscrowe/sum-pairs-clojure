7fc66b4b809b2fbe389832ce8debdcad09d3c908
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-19 10:32:23 -0700

    added some tests, not passing all tests

--- src/sum_pairs/core.clj
+++ src/sum_pairs/core.clj
@@ -23,18 +23,33 @@
 (defn add-divisibles [k pairs]
   (reduce + (map (partial is-mod-k k) pairs)))
 
-(deftest happy-path
-  (def input [1 3 2 6 1 2])
-  (def k 3)
+(deftest pairing
+  (def input [1 2 1])
   (def pairs (create-pairs input 0 []))
-  (is (= pairs) 5))
+  (is (= pairs [3 3]))
+  )
+
+(deftest pairing2
+  (def input [2 1 1])
+  (def pairs (create-pairs input 0 []))
+  (is (= pairs [3 3]))
+  )
+
+
+(deftest pairing3
+  (def input [1 1 2])
+  (def pairs (create-pairs input 0 []))
+  (is (= pairs [3 3]))
+  )
+
+
 
 (defn -main
   "Solves hackerrank problem Sum Pairs"
   [& args]
   (run-all-tests)
-  (def input [5 9 10 7 4])
-  (def k 2)
+  (def input [43 95 51 55 40 86 65 81 51 20 47 50 65 53 23 78 75 75 47 73 25 27 14 8 26 58 95 28 3 23 48 69 26 3 73 52 34 7 40 33 56 98 71 29 70 71 28 12 18 49 19 25 2 18 15 41 51 42 46 19 98 56 54 98 72 25 16 49 34 99 48 93 64 44 50 91 44 17 63 27 3 65 75 19 68 30 43 37 72 54 82 92 37 52 72 62 3 88 82 71])
+  (def k 22)
   (def pairs (create-pairs input 0 []))
   (println (add-divisibles k pairs)))
 
